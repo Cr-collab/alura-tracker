cace70b4d266f06c7562b35de5a1f7e0822f5360
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-01-29 10:48:41 -0300

    Cronometro componetizado e entendendo propriedades

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,79 +1,70 @@
 <template>
   <div class="box">
     <div class="columns">
-         <div class="column is-8" role="form"
-          aria-label="Formulário para criação de uma nova tarefa"
-          >
-            <input 
-              type="text"
-              class="input"
-              placeholder="Qual tarefa você deseja iniciar "
-            />
-         </div>
-         <div class="column">
-           <div class="is-flex is-align-itens-center is-justify-content-space-between">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar "
+        />
+      </div>
+      <div class="column">
+        <div
+          class="is-flex is-align-itens-center is-justify-content-space-between"
+        >
+          <Cronometro :tempoEmSegundos="tempoEmSegundos" />
 
-                <section>
-                  <strong>
-                    {{tempoDecorrido}}
-                  </strong>
-                </section>
+          <button class="button" @click="iniciar">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
 
-                <button class="button" @click="iniciar">
-                  <span class="icon">
-                    <i class="fas fa-play">
-
-                    </i>
-                  </span>
-                    <span> play </span>
-                </button>
-
-                <button class="button" @click="finalizar">
-                  <span class="icon">
-                    <i class="fas fa-stop">
-                      
-                    </i>
-                  </span>
-                    <span> Stop </span>
-                </button>
-           </div>
-         </div>
-    </div> 
+          <button class="button" @click="finalizar">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>Stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Cronometro from './Cronometro.vue'
 
 export default defineComponent({
-    name: 'Formulario',
-    data() {
-         return {
-           tempoEmSegundos: 0,
-           cronometro: 0
-         }
-    },
-    computed:{
-         tempoDecorrido() : string{
-           return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
-         }
-    },
-    methods:{
-      iniciar(){
-           // comecar a contagem 
-          this.cronometro = setInterval(()=>{
-             this.tempoEmSegundos += 1
-           }, 1000)
-      },
-      finalizar(){
-        clearInterval(this.cronometro)
-      }
-
+  name: 'Formulario',
+  components: {
+    Cronometro,
+  },
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
     }
+  },
+
+  methods: {
+    iniciar() {
+      // comecar a contagem
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1
+      }, 1000)
+    },
+    finalizar() {
+      clearInterval(this.cronometro)
+    },
+  },
 })
 </script>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
